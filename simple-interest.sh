618511583cd0685162b607c5996fa83d55be543b
Muhammad Asadullah <masdullah230@gmail.com>
2023-07-23 18:49:18 +0500

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # masdullah230@gmail.com
+   # JoinAsadullah
    # Input:
    # p, principal amount
    # t, time period in years
